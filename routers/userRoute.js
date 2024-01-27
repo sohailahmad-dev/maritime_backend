17a5fe832a71f1d9e26a72a9b1526f5729ba11d8
Noor1236 <slaveofnoor@gmail.com>
2024-01-28 01:22:00 +0500

    Backend

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -2,9 +2,7 @@ import { Router } from "express";
 import { signUpValidation } from '../helper/validation.js'
 import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/userController.js";
 
-
 const userRoute = Router();
-
 
 userRoute.get('/users', getAllUsers);
 userRoute.get('/user/:userId', getUserById);
